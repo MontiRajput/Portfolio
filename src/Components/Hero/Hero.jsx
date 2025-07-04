bac4ac5fed1ce7435eb5fa877337f35d6e2bd951
Monti Rajput <mrrajput7082@gmail.com>
2025-07-04 16:12:44 +0530

    updates

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import profile_img from "../../assets/profile_img.png";
 import "./Hero.css";
+import { Link } from "react-scroll";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 const Hero = () => {
   return (
     <div id="home" className="hero">
@@ -12,8 +14,19 @@ const Hero = () => {
         Hi, I'm a passionate Full-Stack Developer, Solved 1000+ coding Queries.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect With Me</div>
-        <div className="hero-resume">My Resume</div>
+        <Link to="Contact" smooth={true} offset={-100} duration={500}>
+          <div className="hero-connect">Connect with me</div>
+        </Link>
+        <div className="hero-resume">
+          <a
+            href="/resume.pdf"
+            target="_blank"
+            className="anchor"
+            rel="noopener noreferrer"
+          >
+            Get Resume
+          </a>
+        </div>
       </div>
     </div>
   );
